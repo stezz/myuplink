75799abd60a90988daeaffbb871ce69570623e83
Stefano Mosconi <stefano.mosconi@gmail.com>
2024-03-10 13:59:18 +0200

    Better caching and more methods

--- example.py
+++ example.py
@@ -5,7 +5,13 @@ if __name__ == '__main__':
     api = UplinkAPI()
     group_id = api.get_me()
     device_id = api.get_device(group_id)
-    categories = api.get_categories(group_id)
     data = api.get_outdoor_temp(device_id)
-    df = api.save_to_pickle(data)
-    api.plot_data(df)
+    api.plot_data(dataframes=[data], title='Outdoor temperature', labels=['temperature'])
+    brine_in = api.get_brine_in_temp(device_id)
+    brine_out = api.get_brine_out_temp(device_id)
+    # Plot the brine in and out temperatures on the same graph
+    api.plot_data(dataframes=[brine_in, brine_out], title='Brine temperature', labels=['brine in','brine out'])
+    hot_water_charging = api.get_hot_water_charging(device_id)
+    hot_water_top = api.get_hot_water_top(device_id)
+    # Plot the hot water charging and top temperatures on the same graph
+    api.plot_data(dataframes=[hot_water_charging, hot_water_top], title='Hot water temperature', labels=['charging','top'])
--- myuplink.py
+++ myuplink.py
@@ -19,6 +19,35 @@ class UplinkAPI:
         self.load_env_variables()
         self.authenticate()
 
+
+    def use_cache(prefix):
+        "Decorator to cache data to disk and load it from disk if it exists."
+        def decorator(func):
+            def wrapper(self, device_id, days=30):
+                if not os.path.exists('cache'):
+                    os.mkdir('cache')
+                filename = f'cache/{prefix}.pkl'
+                data = func(self, device_id, days)
+                df = pd.DataFrame(data)
+                df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True).dt.tz_convert('Europe/Helsinki')
+                df.set_index('timestamp', inplace=True)
+                if os.path.exists(filename):
+                    print(f'Loading data from cache: {filename}')
+                    df_old = pd.read_pickle(filename)
+                    print(f'Old data: {df_old.shape[0]} rows')
+                    print(f'New data: {df.shape[0]} rows')
+                    df_combined = pd.concat([df_old, df])
+                    df_result = pd.DataFrame(df_combined.to_dict(), index=df_combined.index.unique())
+                    print(f'Combined data: {df_result.shape[0]} rows')
+                    df_result.to_pickle(filename)
+                    return df_result
+                else:
+                    print(f'Saving data to cache: {filename}')
+                    df.to_pickle(filename)
+                return df
+            return wrapper
+        return decorator
+
     def load_env_variables(self):
         load_dotenv()
         self.username = os.getenv('UPLINK_USERNAME')
@@ -58,42 +87,41 @@ class UplinkAPI:
         response = self.session.get(data_url, headers=self.headers)
         return json.loads(response.text)
 
+    @use_cache('outdoor_temp')
     def get_outdoor_temp(self, device_id, days=30):
         return self.fetch_data(device_id, '4', days)
 
+    @use_cache('brine_in_temp')
     def get_brine_in_temp(self, device_id, days=30):
         return self.fetch_data(device_id, '13', days)
 
+    @use_cache('brine_out_temp')
     def get_brine_out_temp(self, device_id, days=30):
         return self.fetch_data(device_id, '14', days)
 
+    @use_cache('hot_water_charging')
     def get_hot_water_charging(self, device_id, days=30):
         return self.fetch_data(device_id, '12', days)
 
-
+    @use_cache('hot_water_top')
     def get_hot_water_top(self, device_id, days=30):
         return self.fetch_data(device_id, '11', days)
 
+    @use_cache('return-line-temp')
     def get_return_line_temp(self, device_id, days=30):
         return self.fetch_data(device_id, '10', days)
 
+    @use_cache('supply-line-temp')
     def get_supply_line_temp(self, device_id, days=30):
         return self.fetch_data(device_id, '8', days)
 
-
-    def save_to_pickle(self, data, filename='cache.pkl'):
-        df = pd.DataFrame(data)
-        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True).dt.tz_convert('Europe/Helsinki')
-        df.set_index('timestamp', inplace=True)
-        if os.path.exists(filename):
-            df_old = pd.read_pickle(filename)
-            df_combined = pd.concat([df_old, df]).drop_duplicates(keep=False)
-            df_combined.to_pickle(filename)
-        else:
-            df.to_pickle(filename)
-        return df
-
-    def plot_data(self, data):
-        data.plot(y='value')
-        plt.ylabel(data['unit'].iloc[0])
+    def plot_data(self, labels, title, dataframes):
+        ax = None
+        for (label, df) in zip(labels, dataframes):
+            # Plot each DataFrame on the same axes (ax) and assign a label for the legend
+            ax = df.plot(y='value', ax=ax, label=f'{label}', grid=True)
+        plt.title(title)
+        plt.ylabel(dataframes[0]['unit'].iloc[0])
+        plt.legend()
         plt.show()
+
